639dbb4fc1c98d810fb8531ffd3f637d1f5b67e6
dario <dario.klepoch@freenet.de>
2020-06-25 14:52:26 +0200

    added function to prevent overheating

--- src/stepper.h
+++ src/stepper.h
@@ -1,5 +1,19 @@
 #include <AccelStepper.h>
 
+
+//motor bottom
+AccelStepper stepper_bottom = AccelStepper(8, D1, D3, D2, D4);
+int stepper_bottom_max = 1000;
+void setup_motor_bottom();
+int min_delay_before_new_pos_bottom = 50;
+
+//motor top
+AccelStepper stepper_top = AccelStepper(8, D5, D7, D6, D0);
+int stepper_top_max = 1000;
+void setup_motor_top();
+int min_delay_before_new_pos_top = 50;
+
+
 //current state (true -> rotating | false -> not rotating)
 bool rotating = false;
 bool move_bottom_right = false;
@@ -16,7 +30,10 @@ bool set_top_maximum = false;
 
 String motor_state;
 
-// Replaces placeholder with toy-state val
+
+/*
+Set the string on the webserver which indicates indicates the current state.
+*/
 String set_rotation(const String& var){
 
   if(var == "STATE"){
@@ -32,10 +49,36 @@ String set_rotation(const String& var){
   return String();
 }
 
+/*
+Disbles all movement instructions to the motors.
+This should help prevent them from overheating.
+*/
 void disable_all_movement(){
+
   rotating = false;
   move_bottom_right = false;
   move_bottom_left  = false;
   move_top_right    = false;
   move_top_left     = false;
+
+  stepper_top.disableOutputs();
+  stepper_bottom.disableOutputs();
+}
+
+/*
+Enables the motors only when a movement is requried.
+This should help prevent them from overheating.
+*/
+void enable_motor_for_movement(){
+
+  if(move_bottom_right || move_bottom_left)
+    stepper_bottom.enableOutputs();
+
+  if(move_top_right || move_top_left)
+    stepper_top.enableOutputs();
+
+  if(rotating){
+    stepper_top.enableOutputs();
+    stepper_bottom.enableOutputs();
+  }
 }

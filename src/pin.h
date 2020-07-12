d6d642ce367c069012af1348bde3302e35772879
dario <dario.klepoch@freenet.de>
2020-07-12 12:37:04 +0200

    changed to arduino definitions

--- src/pin.h
+++ src/pin.h
@@ -1,12 +1,13 @@
+#include <Arduino.h>
 
 extern int LASER_PIN = 017; //D8
 
-extern int stepper_bottom_pin_1 = 005; //D1
-extern int stepper_bottom_pin_2 = 000; //D3
-extern int stepper_bottom_pin_3 = 000; //D2
-extern int stepper_bottom_pin_4 = 002; //D4
+extern int stepper_bottom_pin_1 = D1; //D1
+extern int stepper_bottom_pin_2 = D3; //D3
+extern int stepper_bottom_pin_3 = D2; //D2
+extern int stepper_bottom_pin_4 = D4; //D4
 
-extern int stepper_top_pin_1 = 016; //D5
-extern int stepper_top_pin_2 = 013; //D7
-extern int stepper_top_pin_3 = 012; //D6
-extern int stepper_top_pin_4 = 020; //D0
+extern int stepper_top_pin_1 = D5; //D5
+extern int stepper_top_pin_2 = D7; //D7
+extern int stepper_top_pin_3 = D6; //D6
+extern int stepper_top_pin_4 = D0; //D0

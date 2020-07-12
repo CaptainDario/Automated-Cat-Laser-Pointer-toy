675a3abd1b631e8c841ba20e520ddbc107da169f
dario <dario.klepoch@freenet.de>
2020-07-12 12:30:28 +0200

    added global pin variables

---
+++ src/pin.h
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+extern uint8_t LASER_PIN = 017; //D8
+
+extern uint8_t stepper_bottom_pin_1 = 005; //D1
+extern uint8_t stepper_bottom_pin_2 = 000; //D3
+extern uint8_t stepper_bottom_pin_3 = 000; //D2
+extern uint8_t stepper_bottom_pin_4 = 002; //D4
+
+extern uint8_t stepper_top_pin_1 = 016; //D5
+extern uint8_t stepper_top_pin_2 = 013; //D7
+extern uint8_t stepper_top_pin_3 = 012; //D6
+extern uint8_t stepper_top_pin_4 = 020; //D0

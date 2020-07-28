48e263aa5565832ad44b3903fd1c3bdca9aef01e
dario <dario.klepoch@freenet.de>
2020-07-28 15:47:27 +0200

    added python webserver for hosting the webserver

---
+++ webserver/simple_server.py
@@ -0,0 +1,20 @@
+####################################################
+# This script should be run in:
+#   .\laser_pointer_toy\webserver
+# otherwise it will fail or not work correctly.
+####################################################
+
+
+import http.server 
+import socketserver
+import os
+
+PORT = 8000
+
+os.chdir(os.path.join("..", "data"))
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()

9dd3ffdb83b4f0edbe9a88addde49c1be986fc8c
dario <dario.klepoch@freenet.de>
2020-07-30 00:12:17 +0200

    removed extern because it is default

--- src/pin.h
+++ src/pin.h
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 
-extern int LASER_PIN = 017; //D8
+int LASER_PIN = 017; //D8
 
-extern int stepper_bottom_pin_1 = D1; //D1
-extern int stepper_bottom_pin_2 = D3; //D3
-extern int stepper_bottom_pin_3 = D2; //D2
-extern int stepper_bottom_pin_4 = D4; //D4
+int stepper_bottom_pin_1 = D1; //D1
+int stepper_bottom_pin_2 = D3; //D3
+int stepper_bottom_pin_3 = D2; //D2
+int stepper_bottom_pin_4 = D4; //D4
 
-extern int stepper_top_pin_1 = D5; //D5
-extern int stepper_top_pin_2 = D7; //D7
-extern int stepper_top_pin_3 = D6; //D6
-extern int stepper_top_pin_4 = D0; //D0
+int stepper_top_pin_1 = D5; //D5
+int stepper_top_pin_2 = D7; //D7
+int stepper_top_pin_3 = D6; //D6
+int stepper_top_pin_4 = D0; //D0

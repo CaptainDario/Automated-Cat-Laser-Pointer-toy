7d999a1aa110c548f12d56201dad49f3a4c8c345
CaptainDario <daapplab@gmail.com>
2020-10-30 14:45:22 +0100

    file for saving general information about the software

---
+++ src/about.h
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+//current version
+String version      = "1.2";
+//name of this software
+String software_name = "DaPetToy";
+//the name of the Access Point if no connection to a wifi network is possible
+String ap_ssid      = "DaPetToyAP";
+//the name for this device when connected to a network
+String wifi_name     = "DaPetToy";
